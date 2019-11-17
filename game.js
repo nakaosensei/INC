8a71d29015f295c5c5f1c4709893c7b333dddbfc
nakaosensei <nakaosensei@gmail.com>
2019-11-17 18:33:58 -0300

    final version

--- game.js
+++ game.js
@@ -1,9 +1,12 @@
 window.onload = function() {
 
 	var game = new Phaser.Game(320,480,Phaser.CANVAS,"",{preload:onPreload, create:onCreate, update:update});                
-
+     var holes = []
+     var count = 110
+     sentido = 1
      var player
      var enemy1
+     var startTime
      
 
      function onPreload() {
@@ -21,13 +24,14 @@ window.onload = function() {
 
 	// function to be called when the game has been created
 	function onCreate() {         
+          startTime = new Date().getTime();
           game.physics.startSystem(Phaser.Physics.ARCADE);		
           goFullScreen();          
-          enemy1 = game.add.sprite(100, 100, "enemy");
+          
           enemy1 = game.add.sprite(30, 200, "enemy");
           enemy1.anchor.setTo(0.5)
           game.physics.enable(enemy1, Phaser.Physics.ARCADE);
-
+          
           player = game.add.sprite(160,240,"player");          
           player.anchor.setTo(0.5);          
           game.physics.enable(player, Phaser.Physics.ARCADE);
@@ -38,16 +42,32 @@ window.onload = function() {
                player.body.velocity.x += o.gamma/20;
                player.body.velocity.y += o.beta/20;
           });
-          this.munitionQtde = this.game.add.text(this.game.width-50, this.game.height-50, '5', { fontSize: '32px', fill: '#fff' });		
+          
      }
      
      function killPlayer(){
           player.destroy()
+          endTime = new Date().getTime();
+          diff = Math.round(((endTime-startTime)/60)/60)
+          death = game.add.text(game.width-200, game.height-50, 'Record:'+diff, { fontSize: '8px', fill: '#fff' });		
      }
 
      function update() {
+          if (count<=0){
+               var hole = game.add.sprite(player.x+50*sentido, player.y+50*sentido, "other");
+               hole.anchor.setTo(0.5)
+               game.physics.enable(hole, Phaser.Physics.ARCADE);
+               holes.push(hole)
+               sentido = sentido*-1
+               count=50
+          }else{
+               count = count - 1
+          }
+          for (h of holes){
+               game.physics.arcade.collide(player, h,killPlayer);
+          }    
           game.physics.arcade.collide(player, enemy1,killPlayer);
-          game.physics.arcade.moveToObject(enemy1, player,10);             
+          game.physics.arcade.moveToObject(enemy1, player,15);             
      }
 
 }
